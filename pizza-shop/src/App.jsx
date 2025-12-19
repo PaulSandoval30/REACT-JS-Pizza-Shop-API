15638799864ac8536333bea5f754b701d587fb46
Paul Sandoval <sunghayuki30@gmail.com>
2025-12-19 08:59:01 +0800

    add starting files

---
+++ pizza-shop/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Main from "./components/Main.jsx"
+
+function App() {
+
+  return (
+    <>
+     <div className="max-w-480 mx-auto my-0">
+        <Main />
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ pizza-shop/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='grid place-items-center min-h-screen'>
+      <h1>Pizza Shop</h1>
+    </div>
+  )
+}
+
+export default Main
